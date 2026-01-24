6f88eb77f00d930bf3ab1f934da2443256d67390
alextsikkos <alextsikkos@gmail.com>
2026-01-24 15:05:01 +0000

    Auto-push before ATLAS run

--- engine/enforcement/mdo_hosted_content_filter_enforcers.py
+++ engine/enforcement/mdo_hosted_content_filter_enforcers.py
@@ -301,3 +301,4 @@ def _phishing_action(**kwargs):
 register("MDOBulkSpamAction", _bulk_spam_action)
 register("MDOHighConfidenceSpamAction", _high_conf_spam_action)
 register("MDOPhishingAction", _phishing_action)
+register("MDOBulkComplaintLevelThreshold", _bulk_complaint_level_threshold)
