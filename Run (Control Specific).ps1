92953f5199163f9b17fe4223afc7e8b0e460a546
alextsikkos <alextsikkos@gmail.com>
2026-01-27 09:22:18 +0000

    Auto-push before ATLAS run

--- Run (Control Specific).ps1
+++ Run (Control Specific).ps1
@@ -1,5 +1,3 @@
 ﻿cd C:\Users\Alex\Desktop\Atlas
 
-.\scripts\run-atlas.ps1 `
-  -TenantPath ".\tenants\example_tenant.json" `
-  -OnlyControls "DisableUserConsentToApps"
+.\scripts\run-atlas.ps1 -TenantPath ".\tenants\example_tenant.json" -OnlyControls "DefaultUserRoleReadOtherUsersDisabled,DefaultUserRoleReadBitlockerKeysForOwnedDeviceDisabled"
