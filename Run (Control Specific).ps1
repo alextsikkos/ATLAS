342b714b594155f8048e9af49a1c36a5930f3f5b
alextsikkos <alextsikkos@gmail.com>
2026-01-28 14:56:47 +0000

    Auto-push before ATLAS run

--- Run (Control Specific).ps1
+++ Run (Control Specific).ps1
@@ -1,3 +1,2 @@
 ﻿cd C:\Users\Alex\Desktop\Atlas
-.\scripts\run-atlas.ps1 -TenantPath ".\tenants\example_tenant.json" -OnlyControls `
-"SharePointDefaultSharingRestricted,SharePointDefaultLinkTypeRestricted,SharePointLinkExpirationConfigured,SharePointIdleSessionTimeout,SharePointPreventExternalUsersFromResharingEnabled"
+.\scripts\run-atlas.ps1 -TenantPath ".\tenants\example_tenant.json" -OnlyControls "SharePointDefaultSharingRestricted"
